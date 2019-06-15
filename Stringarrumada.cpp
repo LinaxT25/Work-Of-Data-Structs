4b4e3cfc3d53c054bf025fe97c0fab1fa0447f4b
wildevn <45374307+wildevn@users.noreply.github.com>
2019-06-15 12:37:20 -0300

    Stringarrumada.cpp
    teste para arrumar  a string

---
+++ Stringarrumada.cpp
@@ -0,0 +1,35 @@
+/ Example program
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+string name = "arquivo_1.txt/arquivo_2.txt/arquivo3.txt/";
+string substri;
+//int n = 3;           /// quantidade de barras que tem
+int barras = 0;
+int pos = 0;        /// posição da string
+int tam = -1;      /// tamanho da string
+int ult_pos = -1; /// ultima posição do barra
+
+    for (int i = ult_pos + 1; name[i] != '\0'; i++)
+    {
+        if(name[ult_pos] == '/')
+            pos = i;
+            
+        if(name[i] != '/')
+            tam++;  
+        ult_pos++;
+        if(name[ult_pos] == '/')
+        {
+            barras++;
+            substri.assign(name, pos, tam);
+            cout << substri << endl;
+        }
+            
+    }
+
+    return 0;
+}
