d25024d788499368e5acc596895ab9469b82e289
LinaxT25 <LinaxT25@users.noreply.github.com>
2019-06-15 20:55:53 -0400

    KMP & CalPrefix Changes

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,7 @@
 int main(int argc, char* argv[])
 {
   /* Variáveis do programa */
+  Text object;        // Objeto para chamar os metodos da classe
   string text_lines;  // Pega as linhas do texto & string auxiliar
   string word;        // Palavras a serem buscadas nos arquivos
   string text_files;  // String contendo o nome de cada arquivo verificado
@@ -59,6 +60,7 @@ int main(int argc, char* argv[])
   while(!word_arq.eof())
   {
     getline(word_arq, word); // Pegando a palavra a ser buscada
+    transform(word.begin(),word.end(),word.begin(), ::tolower); // Tranformando a palavra buscada em minuscula
     if(!word_arq.eof()) // Caso seja diferente do final do arquivo, continue
     { 
       for (int i = 1; i <= n; i++)  // Procurando em todos os arquivos
@@ -73,6 +75,7 @@ int main(int argc, char* argv[])
         while(!file_test.eof()) // Enquanto for diferente do final do arquivo texto
         {
           getline(file_test, text_lines);
+          transform(text_lines.begin(),text_lines.end(),text_lines.begin(), ::tolower); // Transformando a linha do arquivo em minuscula
           // MANDAR PARA O KMP
         } 
         file_test.close();
@@ -85,70 +88,4 @@ int main(int argc, char* argv[])
   return 0;
 }
 
-/* Função que realiza do padrão desejado na string que foi recebida pelo arquivo */
-int
-Text::KMP(string Text, string Search)
-{
-  int T_index, S_index; // Indices de Text & Search respectivamente         
-    
-  T_index = 0;  S_index = 0;
-  CalPrefix(Search, Search.size()); // Chamada para criar o array Support
 
-  while((unsigned)T_index <= Text.size())
-    {
-        if(Text[T_index] == Search[S_index])
-        {
-            T_index++;
-            S_index++;
-        }
-        if((unsigned)S_index == Search.size())
-        {
-            cout << "Houve casamento!\n";
-            cout << "Posicao:" << T_index - S_index << endl;
-            S_index = Support[S_index - 1];
-            return 0;
-        }
-        if((unsigned)T_index < Text.size() && Search[S_index] != Text[T_index])
-        {
-            if(S_index == 0)
-                T_index++;
-            else
-                S_index = Support[S_index - 1];
-        }
-    }
-    cout << "Nao houve casamento!\n";
-    return -1;
-}
-
-/* Encontra o maior prefixo que também é sufixo para auxilar no metodo KMP, e 
-   reduzir comparações desnecessárias a serem realizadas pelo mesmo */
-void
-Text::CalPrefix(string Search, int S_size)
-{
-    int k; // Indice e tamanho max da string Support
-    int j; // Indice que localiza as posições de matching
-
-    Support = new int[S_size];
-    Support[0] = 0;
-    j = 0, k = 0;
-
-    while(k < S_size)
-    {
-        if(Search[k + 1] == Search[j]) // Se der match
-        {
-            j++;
-            Support[k] = j;
-            k++;
-        }
-        else
-        {
-            if(j == 0)
-            {
-                Support[k] = 0;
-                k++;
-            }
-            else
-                j = Support[j - 1]; // J recebe a posição anterior não incrementa i nem j;
-        }    
-    }      
-}
