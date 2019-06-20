b4847a54ba68b75cbfcb5ca0dd3bcaf4599a6c8c
LinaxT25 <LinaxT25@users.noreply.github.com>
2019-06-19 21:18:57 -0400

    Update Class.hpp

--- Class.hpp
+++ Class.hpp
@@ -9,14 +9,14 @@ class Text
 {
 public:
 	int *Support;
-	/* Função que realiza do padrão desejado na string que foi recebida pelo arquivo */
+	/* Function which search the pattern in string */
 	int KMP(string Text, string Search)
 	{
 		int Match = 0;
-		int T_index, S_index; // Indices de Text & Search respectivamente         
+		int T_index, S_index; // Index of Text & Search        
 		
 		T_index = 0;  S_index = 0;
-		CalPrefix(Search, Search.size()); // Chamada para criar o array Support
+		CalPrefix(Search, Search.size()); // Call for creating the string Support
 
 		while((unsigned)T_index <= Text.size())
 		{
@@ -42,12 +42,11 @@ public:
 		return Match;
 	}
 
-	/* Encontra o maior prefixo que também é sufixo para auxilar no metodo KMP, e 
-	reduzir comparações desnecessárias a serem realizadas pelo mesmo */
+	/* Function to creating the string which reduces the comparations to be realized by the KMP */   
 	void CalPrefix(string Search, int S_size)
 	{
-		int k; // Indice e tamanho max da string Support
-		int j; // Indice que localiza as posições de matching
+		int k; // Index and max size of string Support
+		int j; // Index for localize the matching positions in string
 
 		Support = new int[S_size];
 		Support[0] = 0;
@@ -55,7 +54,7 @@ public:
 
 		while(k < S_size)
 		{
-			if(Search[k + 1] == Search[j]) // Se der match
+			if(Search[k + 1] == Search[j]) // If occur match
 			{
 				j++;
 				Support[k] = j;
@@ -69,7 +68,7 @@ public:
 					k++;
 				}
 				else
-					j = Support[j - 1]; // J recebe a posição anterior não incrementa i nem j;
+					j = Support[j - 1]; // J recieve the previous position ( i and j doesn't increased)
 			}    
 		}      
 	}
